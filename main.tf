7bcae1b3567ccb83039580281eb9c9e9ebd9ac80
Adedayo Babarinde <dbabarinde@gmail.com>
2023-05-03 17:50:26 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,17 @@
 variable "ami" {}
 variable "region" {}
-variable "instance_type" {}
-variable "secret_key" {} 
+variable "instance_type" {} 
 variable "access_key" {}
-variable "subnet_id" {
-variable "security_group_ids" {
-type    = string
-  default = "ami-0c55b159cbfafe1f0"
-}
+variable "secret_key" {}
+variable "subnet_id" {}
+
 
 resource "aws_instance" "terraform-cloud-demo-server" {
-  ami           = var.ami_id
+  ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
-  vpc_security_group_ids = var.security_group_ids
-
   tags = {
-    Name = "example-instance"
+   Name = "terraform-cloud-demo-server"
   }
 }
+
